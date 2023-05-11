42355d1c350cd25e09a12d4af7f3f8946d15ab05
aapsistemas <130998497+aapsistemas@users.noreply.github.com>
2023-05-11 18:22:43 -0300

    Update initial.py

--- initial.py
+++ initial.py
@@ -2,12 +2,32 @@ import pygame
 
 #criar tela
 pygame.init()
-size = width, height = 400, 400
+size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 
+
+#titulo
+pygame.display.set_caption("game basic")
+
+
+#player
+playerImg = pygame.image.load("img/borb.png")
+playerX = 100
+playerY = 100
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+# loop game
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #color screen
+    screen.fill((60, 72, 107))
+
+    player(playerX,playerY)
+    #update
+    pygame.display.update()
